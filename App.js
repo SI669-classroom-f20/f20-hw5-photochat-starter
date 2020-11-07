a48087d7256874757b86644148631da4f980ffd7
Mark Newman <mwnewman@umich.edu>
2020-11-07 17:45:34 -0500

    starter code

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { LoginScreen } from './LoginScreen';
+import { PeopleScreen } from './PeopleScreen';
+import { ChatScreen } from './ChatScreen';
+import { CameraScreen } from './CameraScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Login"   
+        screenOptions={{
+          headerShown: true
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="People" component={PeopleScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
